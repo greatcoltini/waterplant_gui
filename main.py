7ece3871966a38c520f9687a5553c7458a3d9f67
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-08-05 17:33:40 -0400

    -- tons of efficiency work

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import datetime
 import tkinter as tk
+from tkinter import ttk
 import os
 import time
 
 plant_file = open("plant_database.pf", "w")
-current_time = datetime.datetime.now()
-plant_file.write(str(current_time.year) + "-" + str(current_time.month) + "-" + str(current_time.day) + "\n")
 
 PLANT_ANALYZERS = [
     "UV-AIT3",
@@ -30,14 +29,17 @@ def submit_analyzers():
 
     time.sleep(1)
 
-    os.startfile("plant_database.pf")
+    change_state(2)
 
 
 # Method to write information inputted by user
 def write_analyzer():
+    current_time = datetime.datetime.now()
+
     analyzer_info = [analyzer_name_entry, analyzer_reading_entry, analyzer_recording1_entry,
                      analyzer_recording2_entry]
     analyzer_string = ""
+    analyzer_string += str(current_time.year) + "/" + str(current_time.month) + "/" + str(current_time.day) + "\t"
     for info in analyzer_info:
         analyzer_string += info.get() + "\t"
     plant_file.write(analyzer_string + "\n")
@@ -45,7 +47,8 @@ def write_analyzer():
     for info in analyzer_info:
         info.delete(0, tk.END)
 
-def changeAnalyzer(event):
+
+def change_analyzer(event):
     analyzer_name_entry.config(state="normal")
     analyzer_name_entry.delete(0, tk.END)
     analyzer_name_entry.insert(0, plant_analyzers_var.get())
@@ -54,14 +57,12 @@ def changeAnalyzer(event):
 
 # Command that changes the states upon button press
 def change_state(event):
-    if event == 0:
-        state_switch(True, analyzer_page)
-        state_switch(False, uv_page)
-    elif event == 1:
-        state_switch(True, uv_page)
-        state_switch(False, analyzer_page)
-    else:
-        pass
+    for page in pages:
+        state_switch(True, page)
+    state_switch(False, pages[event])
+
+    if event == 2:
+        table_generation()
 
 
 # Controls the state of the required page
@@ -72,6 +73,44 @@ def state_switch(state, page):
     else:
         for item in page:
             item.pack()
+
+
+# Table generation
+# OPENS THE GENERATED PLANT FILE FOR ANALYZERS
+# READS THEM INTO A TABLE BASED ON DATE
+# COLUMN FOR DATE | ANALYZER NAME | READING | RESIDUAL 1 | RESIDUAL 2
+def table_generation():
+    # necessary to refresh table
+    analyzer_table.delete(*analyzer_table.get_children())
+
+    analyzer_data = open("plant_database.pf", "r")
+    analyzer_table['columns'] = ('Date', 'Analyzer', 'Reading', 'Residual 1', 'Residual 2')
+
+    # format our column
+    analyzer_table.column("#0", width=0, stretch=tk.NO)
+    analyzer_table.column("Date", anchor=tk.CENTER, width=80)
+    analyzer_table.column("Analyzer", anchor=tk.CENTER, width=80)
+    analyzer_table.column("Reading", anchor=tk.CENTER, width=80)
+    analyzer_table.column("Residual 1", anchor=tk.CENTER, width=80)
+    analyzer_table.column("Residual 2", anchor=tk.CENTER, width=80)
+
+    # Create Headings
+    analyzer_table.heading("#0", text="", anchor=tk.CENTER)
+    analyzer_table.heading("Date", text="Date", anchor=tk.CENTER)
+    analyzer_table.heading("Analyzer", text="Analyzer", anchor=tk.CENTER)
+    analyzer_table.heading("Reading", text="Reading", anchor=tk.CENTER)
+    analyzer_table.heading("Residual 1", text="Residual 1", anchor=tk.CENTER)
+    analyzer_table.heading("Residual 2", text="Residual 2", anchor=tk.CENTER)
+
+    counter = 0
+    for line in analyzer_data:
+        section = line.split("\t")
+        analyzer_table.insert(parent='', index='end', iid=str(counter), text='', values=(section[0], section[1], section[2],
+                                                                                section[3], section[4]))
+        counter += 1
+
+    analyzer_table.pack()
+    analyzer_data.close()
 
 
 # Generation of the GUI for the water plant
@@ -139,8 +178,18 @@ finalize_button.pack()
 plant_analyzers_var = tk.StringVar()
 plant_analyzers_var.set(PLANT_ANALYZERS[0])
 
-analyzer_menu = tk.OptionMenu(window, plant_analyzers_var, *PLANT_ANALYZERS, command=changeAnalyzer)
+analyzer_menu = tk.OptionMenu(window, plant_analyzers_var, *PLANT_ANALYZERS, command=change_analyzer)
 analyzer_menu.pack()
+
+display_previous_recordings = tk.Button(text="Show Previous Recordings", command=lambda: change_state(2))
+display_previous_recordings.pack()
+
+
+# components for analyzer history page
+analyzer_table = ttk.Treeview(window)
+
+
+back_button = tk.Button(text="Back", command=lambda: change_state(1))
 
 # Container of all analyzer page elements
 analyzer_page = [
@@ -152,11 +201,24 @@ analyzer_page = [
     analyzer_submit_button,
     finalize_button,
     analyzer_label,
-    analyzer_menu
+    analyzer_menu,
+    display_previous_recordings
+]
+
+analyzer_previous_page = [
+    analyzer_table,
+    back_button
 ]
 
 uv_page = [
 
+]
+
+# container of all containers
+pages = [
+    uv_page,
+    analyzer_page,
+    analyzer_previous_page
 ]
 
 
